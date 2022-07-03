50a100380101823f1d748839a459962ca5a665cf
pturriagol <pturriagol@consultwithcase.com>
2022-07-03 01:00:15 -0400

    creating-bestseller-list-app: started some api stuff and can deploy now

---
+++ get-bestseller-list/app.js
@@ -0,0 +1,51 @@
+const AWS = require("aws-sdk");
+const axios = require("axios");
+
+AWS.config.apiVersions = {
+  dynamodb: "2012-08-10",
+};
+AWS.config.update({
+  region: "us-east-1",
+});
+
+/**
+ * Begin execution of GetKMP Lambda Function
+ */
+async function start(event) {
+  try {
+    //get nyt bestseller books api key/secret
+    //call axios
+    //look through it with consoles maybe
+    //put data in bucket
+  } catch (err) {
+    console.info(err);
+    throw err;
+  }
+} // start
+
+/**
+ *
+ * Event doc: https://docs.aws.amazon.com/apigateway/latest/
+ *   developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+ * @param {Object} event - API Gateway Lambda Proxy Input Format
+ *
+ * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html
+ * @param {Object} context
+ *
+ * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
+ * @returns {Object} object - API Gateway Lambda Proxy Output Format
+ *
+ */
+async function handler(event) {
+  let response;
+  try {
+    response = await start(event);
+  } catch (err) {
+    console.info(err);
+    return err;
+  }
+
+  return response;
+}
+
+module.exports = { handler };
